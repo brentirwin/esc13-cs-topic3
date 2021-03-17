cc2f3eeb875e76f8490dd4b009983bcced4bd342
Brent Irwin <brent@brentirwin.tv>
2021-03-17 11:04:28 -0500

    Generate random integer
    Student B's Pull Request

--- script.py
+++ script.py
@@ -1,11 +1,13 @@
 # ESC13 - CS Topic 3
 # Brent Irwin
 
+import random
+
 # Get input integers
 print("Enter one integer: ")
-val1 = input()
+val1 = int(input())
 print("Enter another integer: ")
-val2 = input()
+val2 = int(input())
 
 # Sort integers
 if val1 > val2:
@@ -13,8 +15,8 @@ if val1 > val2:
     val1 = val2
     val2 = tmp
 
-# FIXME: Get random value between val1 and val2
-val3 = "FIXME"
+# Get random value between val1 and val2
+val3 = random.randint(val1, val2)
 
 # Output
 print("A random integer between %s and %s is %s."%(val1, val2, val3))
